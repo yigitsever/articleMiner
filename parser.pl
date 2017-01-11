4f3c2df19313ee7fd0cb707adb3ddcf7e2d4c70c
Yigit Sever <yigit.sever@tedu.edu.tr>
2017-01-11 18:53:42 +0300

    First parse

--- parser.pl
+++ parser.pl
@@ -1,6 +1,19 @@
 use strict;
 use warnings;
 
-use LWP::Simple;
+use HTML::TokeParser::Simple;
 use Data::Dumper;
+use utf8;
 
+my $parser = HTML::TokeParser::Simple->new(url => 'http://www.milliyet.com.tr/gaziantep-te-patlama--gundem-2237209/');
+
+my @contents;
+
+while ( my $div = $parser->get_tag('div') ) {
+	my $id = $div->get_attr('itemprop');
+	next unless defined($id) and $id eq 'articleBody';
+
+	push @contents, $div;
+}
+
+print Dumper \@contents;
