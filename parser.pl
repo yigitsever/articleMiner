0314306f1ee85a48e10a3590cdfd048054c893dc
yigit sever <yigit.sever@tedu.edu.tr>
2017-01-13 01:40:11 +0300

    Backup for  bug fix

--- parser.pl
+++ parser.pl
@@ -2,16 +2,33 @@ use strict;
 use warnings;
 
 use LWP::Simple;
+use Tie::File; # Not required at all but i like using this
+use Smart::Comments;
 use Data::Dumper;
 
 use utf8;
 
+my ( $dataFileName ) = @ARGV;
+
+if ( not defined $dataFileName ) {
+	die "Need data file name";
+}
+
+tie my @dataRows, 'Tie::File', $dataFileName or die "Cannot initialize TieFile, $!";
+
 open(my $fh, '>:encoding(UTF-8)', 'output') or die "Could not open bla bla $!";
 
-my $url = "http://www.milliyet.com.tr/reyhanli-da-bir-evde-patlama-oldu-gundem-2273965/";
-my $content = get($url);
+for my $row (@dataRows) {
+	my @urlPair = split ",", $row;
+	my $url = $urlPair[1];
+	my $content = get($url);
 
-my $text = $1 if ($content =~ /<div itemprop=\"articleBody\">(.+?)<\/div>/gi);
+	my $text = $1 if ($content =~ /<div itemprop=\"articleBody\">(.+?)<\/div>/gi);
+	if (not defined $text) {
+		$text = $url . "\t<===";
+	} else {
+		$text =~ s/<.+?>//g;
+	}
 
-$text =~ s/<.+?>//g;
-print $fh $text;
+	print $fh "$text\n";
+}
