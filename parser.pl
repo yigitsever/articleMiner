d46a5197a710646c85063797ef4a8f300d9c4c23
yigit sever <yigit.sever@tedu.edu.tr>
2017-01-13 02:14:55 +0300

    Working articleBody extract

--- parser.pl
+++ parser.pl
@@ -1,19 +1,17 @@
 use strict;
 use warnings;
 
-use HTML::TokeParser::Simple;
+use LWP::Simple;
 use Data::Dumper;
+
 use utf8;
 
-my $parser = HTML::TokeParser::Simple->new(url => 'http://www.milliyet.com.tr/gaziantep-te-patlama--gundem-2237209/');
+open(my $fh, '>:encoding(UTF-8)', 'output') or die "Could not open bla bla $!";
 
-my @contents;
+my $url = "http://www.milliyet.com.tr/reyhanli-da-bir-evde-patlama-oldu-gundem-2273965/";
+my $content = get($url);
 
-while ( my $div = $parser->get_tag('div') ) {
-	my $id = $div->get_attr('itemprop');
-	next unless defined($id) and $id eq 'articleBody';
+my $text = $1 if ($content =~ /<div itemprop=\"articleBody\">(.+?)<\/div>/gi);
 
-	push @contents, $div;
-}
-
-print Dumper \@contents;
+$text =~ s/<.+?>//g;
+print $fh $text;
